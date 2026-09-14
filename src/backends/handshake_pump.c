cebbb0c7ae4860ec6810c6cbd6b9791577c123b0
pos <pgousdal@users.noreply.github.com>
2026-09-14 19:13:23 +0200

    M2.1c: follow engine state after record ack

--- src/backends/handshake_pump.c
+++ src/backends/handshake_pump.c
@@ -8,6 +8,23 @@ static int pump_valid(const AmTLS_HandshakePump *pump)
         && pump->transport != 0;
 }
 
+static AmTLS_BackendResult result_for_state(AmTLS_EngineState state)
+{
+    switch (state) {
+    case AMTLS_ENGINE_APPLICATION:
+        return AMTLS_BACKEND_OK;
+    case AMTLS_ENGINE_CLOSED:
+        return AMTLS_BACKEND_CLOSED;
+    case AMTLS_ENGINE_ERROR:
+        return AMTLS_BACKEND_ERROR;
+    case AMTLS_ENGINE_SEND_RECORD:
+        return AMTLS_BACKEND_WANT_WRITE;
+    case AMTLS_ENGINE_RECV_RECORD:
+        return AMTLS_BACKEND_WANT_READ;
+    }
+    return AMTLS_BACKEND_ERROR;
+}
+
 AmTLS_BackendResult amtls_handshake_pump_step(AmTLS_HandshakePump *pump)
 {
     AmTLS_EngineState state;
@@ -23,13 +40,9 @@ AmTLS_BackendResult amtls_handshake_pump_step(AmTLS_HandshakePump *pump)
 
     switch (state) {
     case AMTLS_ENGINE_APPLICATION:
-        return AMTLS_BACKEND_OK;
-
     case AMTLS_ENGINE_CLOSED:
-        return AMTLS_BACKEND_CLOSED;
-
     case AMTLS_ENGINE_ERROR:
-        return AMTLS_BACKEND_ERROR;
+        return result_for_state(state);
 
     case AMTLS_ENGINE_SEND_RECORD:
         if (pump->engine_ops->send_buffer == 0
@@ -50,7 +63,7 @@ AmTLS_BackendResult amtls_handshake_pump_step(AmTLS_HandshakePump *pump)
             return AMTLS_BACKEND_CLOSED;
         }
         pump->engine_ops->send_ack(pump->engine, (size_t)n);
-        return AMTLS_BACKEND_WANT_WRITE;
+        return result_for_state(pump->engine_ops->state(pump->engine));
 
     case AMTLS_ENGINE_RECV_RECORD:
         if (pump->engine_ops->recv_buffer == 0
@@ -71,7 +84,7 @@ AmTLS_BackendResult amtls_handshake_pump_step(AmTLS_HandshakePump *pump)
             return AMTLS_BACKEND_CLOSED;
         }
         pump->engine_ops->recv_ack(pump->engine, (size_t)n);
-        return AMTLS_BACKEND_WANT_READ;
+        return result_for_state(pump->engine_ops->state(pump->engine));
     }
 
     return AMTLS_BACKEND_ERROR;
